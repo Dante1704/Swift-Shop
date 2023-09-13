f2ab9ef38f6f8ba38a4d55f3d3ac6ff12260a6ba
fsponton <francisco.sponton@gmail.com>
2023-09-13 16:37:42 -0300

    se agrego retornar el error especifico

--- src/config/enviroments.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config({ path: "././.env" })
-const { stringToArray } = require("../helpers")
-
-module.exports = {
-    ENTITIES: stringToArray(process.env.ENTITIES)
-}   
--- src/server.js
+++ src/server.js
@@ -6,10 +6,15 @@ server.use(express.json())
 
 server.use(require('./routes'))
 
-
-server.use((req, res) => {
+server.use((err, req, res, next) => {
+    if (err.name === "clientError" || err.name === "modelError") {
+        return res.status(401).send({
+            error: true,
+            errorName: err.name,
+            message: err.message
+        })
+    }
     res.status(404).send("Bad Request")
 })
-
 
 module.exports = server
